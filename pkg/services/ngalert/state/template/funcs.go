d815fa3e0659540a46268d7c899e4f9039f025dd
Oleg <oleg@makarenk.ooo>
2023-06-28 15:36:21 +0300

    Added toTime function to Alert template

--- pkg/services/ngalert/state/template/funcs.go
+++ pkg/services/ngalert/state/template/funcs.go
@@ -2,10 +2,16 @@ package template
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"math"
 	"net/url"
 	"regexp"
+	"strconv"
 	"text/template"
+	"time"
+
+	"github.com/prometheus/common/model"
 )
 
 type query struct {
@@ -21,6 +27,7 @@ var (
 		"removeLabels":    removeLabelsFunc,
 		"removeLabelslRe": removeLabelsReFunc,
 		"tableLink":       tableLinkFunc,
+		"toTime":          toTimeFunc,
 	}
 )
 
@@ -88,4 +95,43 @@ func tableLinkFunc(data string) string {
 	datasource := url.QueryEscape(q.Datasource)
 	expr := url.QueryEscape(q.Expr)
 	return fmt.Sprintf(`/explore?left={"datasource":%[1]q,"queries":[{"datasource":%[1]q,"expr":%q,"instant":true,"range":false,"refId":"A"}],"range":{"from":"now-1h","to":"now"}}`, datasource, expr)
+}
+
+func convertToFloat(i interface{}) (float64, error) {
+	switch v := i.(type) {
+	case float64:
+		return v, nil
+	case string:
+		return strconv.ParseFloat(v, 64)
+	case int:
+		return float64(v), nil
+	case uint:
+		return float64(v), nil
+	case int64:
+		return float64(v), nil
+	case uint64:
+		return float64(v), nil
+	default:
+		return 0, fmt.Errorf("can't convert %T to float", v)
+	}
+}
+
+func floatToTime(v float64) (*time.Time, error) {
+	if math.IsNaN(v) || math.IsInf(v, 0) {
+		return nil, errors.New("value is NaN or Inf")
+	}
+	timestamp := v * 1e9
+	if timestamp > math.MaxInt64 || timestamp < math.MinInt64 {
+		return nil, fmt.Errorf("%v cannot be represented as a nanoseconds timestamp since it overflows int64", v)
+	}
+	t := model.TimeFromUnixNano(int64(timestamp)).Time().UTC()
+	return &t, nil
+}
+
+func toTimeFunc(data interface{}) (*time.Time, error) {
+	v, err := convertToFloat(data)
+	if err != nil {
+		return nil, err
+	}
+	return floatToTime(v)
 }
